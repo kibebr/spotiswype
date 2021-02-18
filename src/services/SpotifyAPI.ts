d5a388b8c4704be8b93f8d98c4f43c915ec063ad
kibebr <0763722@students.dadeschools.net>
2021-02-18 20:03:47 +0000

    api request to get recommended songs is done!

--- src/services/SpotifyAPI.ts
+++ src/services/SpotifyAPI.ts
@@ -16,6 +16,7 @@ export const getProfile = (accessToken: string) => new Promise<unknown>((resolve
 })
 
 export const getSavedTracks = (accessToken: string) => new Promise<unknown>((resolve, reject) => {
+  console.log('getSavedTracks called!')
   const request = new XMLHttpRequest()
 
   request.open('GET', 'https://api.spotify.com/v1/me/tracks')
@@ -49,16 +50,38 @@ export const getArtist = (accessToken: string) => (artistId: string) => new Prom
   request.send()
 })
 
-export const getRecommendedSongs = (accessToken: string) => (seedTracks: string[]) => (seedGenres: string[]) => (seedArtists: string[]) =>
-  new Promise<string>((resolve, reject) => {
-    resolve('a')
+export const getSeveralArtists = (accessToken: string) => (artistsIds: string[]) => new Promise<unknown>((resolve, reject) => {
+  const request = new XMLHttpRequest()
+
+  request.open('GET', `https://api.spotify.com/v1/artists?ids=${artistsIds.join()}`)
+  request.setRequestHeader('Authorization', `Bearer ${accessToken}`)
+
+  request.onload = () => {
+    if (request.status >= 400 && request.status <= 500) {
+      reject(Error(JSON.parse(request.responseText)))
+    } else {
+      resolve(JSON.parse(request.responseText))
+    }
+  }
+
+  request.send()
+})
+
+export const getRecommendedSongs = (accessToken: string) => (seedTracks: string[]) => (seedArtists: string[]) => (seedGenres: string[]) =>
+  new Promise<unknown>((resolve, reject) => {
+    console.log(accessToken, seedTracks, seedArtists, seedGenres)
     const request = new XMLHttpRequest()
     
-    request.open('GET', `https://api.spotify.com/v1/artists/${accessToken}`)
-      request.setRequestHeader('Authorization', `Bearer ${accessToken}`)
+    const _seedGenres = seedGenres.join().replace(' ', '+')
+    request.open(
+      'GET', 
+      `https://api.spotify.com/v1/recommendations?seed_tracks=${seedTracks.join()}&seed_artists=${seedArtists.join()}&seedGenres=${_seedGenres}`
+    )
+    request.setRequestHeader('Authorization', `Bearer ${accessToken}`)
 
     request.onload = () => {
       if (request.status >= 400 && request.status <= 500) {
+        console.error(JSON.parse(request.responseText))
         reject(Error(JSON.parse(request.responseText)))
       } else {
         resolve(JSON.parse(request.responseText))
--- src/spotifyapi.ts
+++ src/spotifyapi.ts
@@ -1,11 +1,11 @@
-import { map as temap, of, ap, chainEitherKW, chain  tryCatch, tryCatchK, TaskEither } from 'fp-ts/TaskEither'
+import { map as temap, of, ap, chain, chainEitherKW, tryCatch, TaskEither, Do, bind } from 'fp-ts/TaskEither'
 import { map as amap, takeLeft } from 'fp-ts/Array'
 import { pipe, flow } from 'fp-ts/function'
 import { User } from './index'
 import { 
   getProfile, 
   getSavedTracks, 
-  getArtist,
+  getSeveralArtists,
   getRecommendedSongs
 } from './services/SpotifyAPI'
 import { getHashParams } from './utils'
@@ -16,13 +16,21 @@ const profileSpotifyResponse = type({
   name: string
 })
 
+const getSeveralArtistsResponseV = type({
+  artists: array(type({
+    genres: array(string)
+  }))
+})
+
 const artistResponseV = type({
   id: string
 })
 
 const itemResponseV = type({
-  artists: array(artistResponseV),
-  id: string
+  track: type({
+    artists: array(artistResponseV),
+    id: string
+  })
 })
 
 const getSavedTracksResponse = type({
@@ -33,42 +41,46 @@ type ItemResponse = TypeOf<typeof itemResponseV>
 type ArtistResponse = TypeOf<typeof artistResponseV>
 type SavedTracksResponse = TypeOf<typeof getSavedTracksResponse>
 
-const get5Items: (t: SavedTracksResponse) => ItemResponse[] = flow(prop('items'), takeLeft(5)) 
+const get5Items: (t: SavedTracksResponse) => ItemResponse[] = flow(prop('items'), takeLeft(2)) 
 
-const get5TracksIds = (savedTracks: SavedTracksResponse) => string[] = flow(
+const get5Artists: (savedTracks: SavedTracksResponse) => ArtistResponse[] = flow(
   get5Items,
-  amap(item => item.id)
+  amap(item => item.track.artists[0])
 )
 
-const get5ArtistsIds: (savedTracks: SavedTracksResponse) => string[] = flow(
-  prop('items'),
-  takeLeft(5),
-  amap(item => item.artists[0].id)
-)
-
-
-/* const getSong = (href: string): Promise<unknown> => new Promise<unknown>((resolve, reject) => { */
-/*   resolve({ play: '' }) */
-/* }) */
-
-/* export const getUserData = (ate: string): TaskEither<string | Errors, any> => pipe( */
-/*   tryCatch( */
-/*     () => getProfile(ate), */
-/*     () => 'Could not fetch user.' */
-/*   ), */
-/*   chainEitherKW(profileSpotifyResponse.decode), */
-/*   map(({ name }) => ({ name, savedSongs: [], token: 't' })) */
-/* ) */
-
-export const getRecommendations = ({ token }: User): TaskEither<unknown, string> => pipe(
-  of(getRecommendedSongs),
-  ap(of(token)),
-  ap(of(['a'])),
-  ap(of(['a'])),
-  ap(of(['a'])),
-  chain(promise => tryCatch(
-    () => promise,
-    (): unknown => ''
+// i put pipe on the last ap because u gonna have to decode it later
+export const getRecommendations = ({ token }: User): TaskEither<unknown, any> => pipe(
+  Do,
+  bind('favoriteTracks', () => tryCatch(
+    () => getSavedTracks(token),
+    (): unknown => 'Uh oh, there was a problem while fetching for your favorite tracks! Please contact the developer.'
+  )),
+  chainEitherKW(({ favoriteTracks }) => getSavedTracksResponse.decode(favoriteTracks)),
+  chain((savedTracks) => pipe(
+    of(getRecommendedSongs),
+    ap(of(token)),
+    ap(of(pipe(
+      get5Items(savedTracks),
+      amap(flow(prop('track'), prop('id')))
+    ))),
+    ap(of(pipe(
+      get5Artists(savedTracks), 
+      amap(prop('id'))
+    ))),
+    ap(pipe(
+      get5Artists(savedTracks),
+      amap(prop('id')),
+      (ids) => tryCatch(
+        () => getSeveralArtists(token)(ids),
+        (): unknown => 'Uh oh, there was a problem while fetching for the artists! Please contact the developer.'
+      ),
+      chainEitherKW(getSeveralArtistsResponseV.decode),
+      temap(flow(prop('artists'), amap(a => a.genres[0])))
+    )),
+    chain(promise => tryCatch(
+      () => promise,
+      (err): unknown => 'Uh oh, there was a problem while fetching for the recommended songs! Please contact the developer.'
+    ))
   ))
 )
 
